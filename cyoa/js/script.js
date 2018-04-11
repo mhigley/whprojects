e8c238606e3fe23ef50c63f2f386fd7843bec0a5
mhigley <mhigley@bitwiseindustries.com>
2018-04-11 13:26:44 -0700

    scene1 of cyoa

--- cyoa/js/script.js
+++ cyoa/js/script.js
@@ -1,39 +1,49 @@
-// alert('test');
+var start = document.querySelector('button[class="start"]');
+start.addEventListener('click', startGame);
 
+var story = document.getElementById('story');
 var options = document.getElementById('options');
 
-var scene1 = [
-    'As you open the door in front of you, a distant "popping" sound rings throughout the hall as all the lights go dark. You freeze and instinctively throw your hands out in front of you, waving them aimlessly to shield you from harm.',
-    'flick on your lighter',
-    'search for a light switch',
-    'navigate through the dark'
-];
-var scene2 = [
-    'You fumble through your pockets, frantically digging to find your lighter. As you rotate it in your hand feeling for the familiar clamshell lid, you properly orient it between your fingers, thumb the lid open with your left hand and instinctively flick your right thumb down creating a sudden burst of sparks. It lights and casts a shadowy glow around you.',
-    'step into the dark room ahead',
-    'close the door and backtrack down the hall',
-    'toss the lighter ahead of you illumiating the room'
-];
-
-function scenePlay(scene){
-    options.innerHTML = '';
-    for(var i = 1; i < scene.length; i++){
-        options.innerHTML += makeBtns(scene[i]);
+var scene1 = {
+    options: [
+        {
+            title: 'one',
+            action: 'you open the left door',
+            outcome: 'the door swings open revealing a rat'
+        },
+        {
+            title: 'two',
+            action: 'you open the middle door',
+            outcome: 'the door swings open and youre blasted with fire'
+        },
+        {
+            title: 'three',
+            action: 'you open the right door',
+            outcome: 'the door swings open and you walk iinto the next room'
+        }
+    ],
+    story: function(){
+        return '<p>you enter a room and see ' + this.options.length + ' doors.</p>'
+    },
+    decision: function(x){
+        return '<p>you walk forwards ' + x + '.</p>';
     }
-    setListeners();
+};
+
+function startGame(){
+    printBoard(scene1);
 }
 
-scenePlay(scene1);
+function printBoard(obj){
+    story.innerHTML = obj.story();
+    
+    obj.options.forEach(function(btnText, index){
+        options.innerHTML += '<button data-num="'+ index +'">'+ btnText.title +'</button>';
+    });
 
-function makeBtns(str){
-    return `<button type="button">${str}</button>`;
-}
-
-function setListeners(){
-    document.addEventListener('click', function(e){
-        if(e.target.type == 'button'){
-            scenePlay(scene2);
+    options.addEventListener('click', function(evt){
+        if(evt.target.nodeName == 'BUTTON'){
+            story.innerHTML += obj.decision( obj.options[ parseInt( evt.target.dataset.num ) ].outcome );
         }
     });
 }
-
---
+++ cyoa/js/storyline.js
@@ -0,0 +1,14 @@
+/*
+var scene1 = [
+    'As you open the door in front of you, a distant "popping" sound rings throughout the hall as all the lights go dark. You freeze and instinctively throw your hands out in front of you, waving them aimlessly to shield you from harm.',
+    'flick on your lighter',
+    'search for a light switch',
+    'navigate through the dark'
+];
+var scene2 = [
+    'You fumble through your pockets, frantically digging to find your lighter. As you rotate it in your hand feeling for the familiar clamshell lid, you properly orient it between your fingers, thumb the lid open with your left hand and instinctively flick your right thumb down creating a sudden burst of sparks. It lights and casts a shadowy glow around you.',
+    'step into the dark room ahead',
+    'close the door and backtrack down the hall',
+    'toss the lighter ahead of you illumiating the room'
+];
+*/
